3735a1f4170e7e6c79d27bbee92aed27b79cfffc
Oliver Manheim <omanheim@sas.upenn.edu>
2013-09-28 01:20:31 -0400

    more in models

--- krysis/app/models.py
+++ krysis/app/models.py
@@ -3,13 +3,18 @@ from django.db import models
 class Tweet(models.Model):
    text=models.CharField(max_length=140)
    sender=models.CharField(max_length=50)
-   latitude=models.DecimalField(max_digits=11,decimal_places=8,blank=True)
-   longitude=models.DecimalField(max_digits=11,decimal_places=8,blank=True)
-   date=models.DateTimeField(auto_now_add=True)
+   latitude=models.DecimalField(max_digits=11,decimal_places=8,null=True)
+   longitude=models.DecimalField(max_digits=11,decimal_places=8,null=True)
+   keywords=models.ManyToManyField('Keyword')
+   date=models.DateTimeField(auto_now_add=True,null=True)
 
 class Text(models.Model):
    text=models.CharField(max_length=400)
    sender=models.CharField(max_length=10)
-   latitude=models.DecimalField(max_digits=11,decimal_places=8,blank=True)
-   longitude=models.DecimalField(max_digits=11,decimal_places=8,blank=True)
-   date=models.DateTimeField(auto_now_add=True)
+   latitude=models.DecimalField(max_digits=11,decimal_places=8,null=True)
+   longitude=models.DecimalField(max_digits=11,decimal_places=8,null=True)
+   keywords=models.ManyToManyField('Keyword')
+   date=models.DateTimeField(auto_now_add=True,null=True)
+
+class Keyword(models.Model):
+   word=models.CharField(max_length=100)
